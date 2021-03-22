51f58bf1f7a71b6eb06c693820e6f8aac3b00061
Daniel Lopez <70593155+daniloperama2006@users.noreply.github.com>
2021-03-21 19:49:34 -0500

    Add files via upload

---
+++ saludoConTuNombre.js
@@ -0,0 +1,6 @@
+//prompt es un comando para ingresar un dato
+var nombre = prompt(`¿Cuál es tu nombre?`)
+    //para abrir estas comillas invertidas solo dale alt+96
+    /* //estoy invocando la variable nombre para que aplique al nombre 
+    se le escriba// */
+console.log(`Hola ${nombre}, esto es JavaScript`)
